e4019ec3f1c949788b2750cf4f04f47152faab84
Arnav Jain <arnavj0610@gmail.com>
2024-08-26 19:23:21 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,12 @@
-window.addEventListener('scroll', function() {
-    const hiddenNav = document.getElementById('hidden-nav');
-    if (window.scrollY > 300) {
-      hiddenNav.classList.add('nav-visible');
-    } else {
-      hiddenNav.classList.remove('nav-visible');
-    }
-  });
-  
-  
-  function toggleresponsive() {
-    const ham = document.querySelector('#nav-icon3')
-    const nav = document.querySelector('.mobile-nav')
-    ham.classList.toggle('open')
-    const wrapper = document.querySelector('.hamburger-wrapper')
-    wrapper.classList.toggle('wrapper-active')
-    nav.classList.toggle('mobile-active')
-  }
-  
-  
+const lenis = new Lenis()
+
+lenis.on('scroll', (e) => {
+console.log(e)
+})
+
+function raf(time) {
+lenis.raf(time)
+requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
